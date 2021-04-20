737fb3780dff2ce06f1cf00b5b1e952feadf879b
daniporre <daniporre@hotmail.es>
2021-04-21 01:20:10 +0200

    proyecto spring inicial

---
+++ Backend/weRaceAsOneF1/src/main/java/ual/dra/weRaceAsOneF1/WeRaceAsOneF1Application.java
@@ -0,0 +1,13 @@
+package ual.dra.weRaceAsOneF1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeRaceAsOneF1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WeRaceAsOneF1Application.class, args);
+	}
+
+}
---
+++ Backend/weRaceAsOneF1/src/test/java/ual/dra/weRaceAsOneF1/WeRaceAsOneF1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ual.dra.weRaceAsOneF1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeRaceAsOneF1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
